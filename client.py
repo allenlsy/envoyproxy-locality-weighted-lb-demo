298e76548bd9ce7a86a2014aa63366aa43353b96
Allen Leigh <allenlsy@gmail.com>
2021-06-16 08:55:59 -0700

    calculate failed requests

--- client.py
+++ client.py
@@ -7,11 +7,16 @@ print("Usage: url n_requests")
 url, n_requests = sys.argv[1], int(sys.argv[2])
 
 count = Counter()
+count_fail = 0
 
 for i in range(n_requests):
-    with urllib.request.urlopen(url) as resp:
-        content = resp.read().decode("utf-8")
-        count[content] += 1
+    try:
+        with urllib.request.urlopen(url) as resp:
+            content = resp.read().decode("utf-8")
+            count[content] += 1
+    except:
+        count_fail += 1
 
 for k in count:
     print(f"{k} : {count[k]}, {count[k] / n_requests * 100}%")
+print(f"Failed: {count_fail}")
